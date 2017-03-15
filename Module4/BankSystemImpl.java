91768af0fffbf9629641b0157e8dac06abe385e2
annaolifer <oliferanna@gmail.com>
2017-03-15 15:53:06 +0200

    Module 4

--- Module4/BankSystemImpl.java
+++ Module4/BankSystemImpl.java
@@ -7,23 +7,25 @@ public class BankSystemImpl implements BankSystem{
     User user1 = new User(888,"Andrey", 1000,12,"Deloitte",7000, bank1);
     User user2 = new User(999,"Andrey", 1000,12,"Deloitte",7000, bank1);
 
+    double newBalance;
+
     public void withdrawOfUser(User user, int amount){
-         if (amount < bank1.getLimitOfWithdrawal()){user1.getBalance() - amount;}
+         if (amount < bank1.getLimitOfWithdrawal()){newBalance = user1.getBalance() - amount;}
         else {System.out.println("You have exceeded the limit of withdrawal");}
     }
 
     public void fundUser(User user, int amount) {
-        if (amount < bank1.getLimitOfFunding()){user1.getBalance() + amount;}
+        if (amount < bank1.getLimitOfFunding()){newBalance = user1.getBalance() + amount;}
         else {System.out.println("You have exceeded the limit of funding");}
     }
 
     public void transferMoney(User fromUser, User toUser, int amount){
         if (amount < bank1.getLimitOfWithdrawal() && amount < bank1.getLimitOfFunding()){
-            user1.getBalance() - amount; user2.getBalance() + amount;}
+            newBalance =  user1.getBalance() - amount; newBalance = user2.getBalance() + amount;}
         else {System.out.println("You have exceeded the limit");}
     }
 
-    public void paySalary(User user){user1.getBalance() + user1.getSalary();}
+    public void paySalary(User user){newBalance = user1.getBalance() + user1.getSalary();}
 
     }
 
@@ -41,4 +43,4 @@ public class BankSystemImpl implements BankSystem{
 //    public void paySalary(User user){
 //        System.out.println("Salary payed to Client" + user);
 //    }
-}
+
